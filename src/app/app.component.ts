4adffceeb610799e25288f403f4e3100af119f4c
Dimitris <jim.panag2000@gmail.com>
2023-12-19 19:34:24 +0200

    Changes #1 1. Added the SearchBar and PageList components and made them useable in the app.component without the routing method 2. Added the wikipedia service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PageListComponent } from './page-list/page-list.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SearchBarComponent, PageListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/page-list/page-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-list.component.html',
+  styleUrl: './page-list.component.css'
+})
+export class PageListComponent {
+
+}
